d8980141ea680660c83e2926a5bbeb6b14cf4664
Saed Mohamed <smohamed71@gmail.com>
2022-06-24 19:00:34 -0400

    added huey from main

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,3 @@
 print("yeah, well I can't walk him on The Today Show like that")
+print("Well, now we got to kill him")
+print("Translucent' doesn't even mean 'invisible,' it means 'semi-transparent.")
